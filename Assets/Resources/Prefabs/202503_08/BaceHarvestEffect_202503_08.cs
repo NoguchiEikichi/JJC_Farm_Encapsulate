eb3889d066be8aa56c9b009fcb7aec66459b6789
NoguchiEikichi <aymek.5aikichi@icloud.com>
2023-06-30 10:42:22 +0900

    野口アップロード

---
+++ Assets/Resources/Prefabs/202503_08/BaceHarvestEffect_202503_08.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaceHarvestEffect_202503_08 : BaseHarvestEffect
+{
+
+
+    void Start()
+    {
+        
+    }
+
+    public override void PlayHarvest()
+    {
+        
+    }
+}
